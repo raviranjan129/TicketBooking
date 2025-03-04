5817e0410015c85dc0c88e803e440020371aca17
name <rk6318633@gmail.com>
2025-03-04 17:41:16 +0530

    added seats model

--- backend/src/repositories/flight-repository.js
+++ backend/src/repositories/flight-repository.js
@@ -1,6 +1,6 @@
 const CrudRepository=require('./crud-repository');
-const {Flight}=require('../models')
-
+const {Flight,Airplane,Airport,City}=require('../models')
+const {Sequelize}=require('sequelize');
 class FlightRepository extends CrudRepository{    //parent class is CrudRepository and it expects the model;
     constructor(){
         super(Flight);  //using super keyword you can call the constructor of the parent class;
@@ -9,7 +9,38 @@ class FlightRepository extends CrudRepository{    //parent class is CrudReposito
     async getAllFlights(filter,sort){
         const response=await Flight.findAll({
             where:filter,
-            order:sort
+            order:sort,
+            include:[
+                {
+                model:Airplane,
+                required:true,
+                as:'airplaneDetail'
+            },
+            {
+                model:Airport,
+                required:true,
+                as:'departureAirport',
+                on:{
+                  col1:Sequelize.where(Sequelize.col('Flight.departureAirportId'),"=",Sequelize.col('departureAirport.code'))
+                },
+                include:{
+                    model:City,
+                    required:true
+                }
+            },
+            {
+                model:Airport,
+                required:true,
+                as:'arrivalAirport',
+                on:{
+                  col1:Sequelize.where(Sequelize.col('Flight.arrivalAirportId'),"=",Sequelize.col('arrivalAirport.code'))
+                },
+                include:{
+                    model:City,
+                    required:true
+                }
+            }
+        ]
         });
         return response;
     }
