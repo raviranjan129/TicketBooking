8c677dca6037ee103864ae86d575fd71247a4f9e
name <rk6318633@gmail.com>
2025-03-16 15:14:07 +0530

    added transaction to bookseats

--- backend/src/repositories/flight-repository.js
+++ backend/src/repositories/flight-repository.js
@@ -1,6 +1,6 @@
 const CrudRepository=require('./crud-repository');
 const {Flight,Airplane,Airport,City}=require('../models')
-const {Sequelize}=require('sequelize');
+const {Sequelize, TableHints}=require('sequelize');
 const db=require('../models');
 const {addRowLockOnFlights}=require('./queries')
 class FlightRepository extends CrudRepository{    //parent class is CrudRepository and it expects the model;
@@ -49,17 +49,23 @@ class FlightRepository extends CrudRepository{    //parent class is CrudReposito
 
 
     async updateRemainingSeats(flightId,seats,dec=true ){
+        const transaction =await db.sequelize.transaction();
+       try {
         await db.sequelize.query(addRowLockOnFlights(flightId));
         const flight=await Flight.findByPk(flightId);
-                if(parseInt(dec)){
-                     await flight.decrement('totalSeats',{by:seats});
+                if(+dec){
+                     await flight.decrement('totalSeats',{by:seats},{transaction:transaction});
                     
                 }else{
-                    await flight.increment('totalSeats',{by:seats});
+                    await flight.increment('totalSeats',{by:seats},{transaction:transaction});
                     
                 }
-
+                      await transaction.commit();
                 return flight;
+       } catch (error) {
+        await transaction.rollback();
+        throw error;
+       }
     }
 
 }
